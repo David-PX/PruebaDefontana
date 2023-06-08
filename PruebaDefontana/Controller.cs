8fb6ad30526b4d14add80c43d6c6e18ba2ff9733
David-PX <david0nike@gmail.com>
2023-06-08 12:40:34 -0400

    Add project files.

---
+++ PruebaDefontana/Controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PruebaDefontana.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaDefontana
+{
+    internal static class Controller
+    {
+        public static List<VentaDetalle> consultaDetalleDeVenta(int UltimosDias)
+        {
+            // Aqui agregamos el archivo de configuracion, para poder usarlo en el DbContext llamando al connectionString
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Instanciamos el DbContext y le pasamos nuestra configuracion
+            PruebaContext _context = new PruebaContext(configuration);
+
+            // Realizamos una unica consulta a la bases de datos que incluye las ventas, el detalle de las ventas, los productos y los locales
+            List<VentaDetalle> ventas = _context.VentaDetalles
+                .Include(x => x.IdVentaNavigation)
+                .Include(x => x.IdVentaNavigation.IdLocalNavigation)
+                .Include(x => x.IdProductoNavigation)
+                .Include(x => x.IdProductoNavigation.IdMarcaNavigation)
+                // Aqui agregamos una condicion para filtrar solamente los registros de los días que se indiquen mediante la variable UltimosDias
+                .Where(x => x.IdVentaNavigation.Fecha >= DateTime.Now.AddDays(-UltimosDias))
+                .ToList();
+
+            return ventas;
+        }
+    }
+}
---
+++ PruebaDefontana/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using PruebaDefontana;
+using PruebaDefontana.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Traemos desde el controlador toda la data que estaremos utilizando en las consultas.
+        List<VentaDetalle> datos = Controller.consultaDetalleDeVenta(30);
+
+        //El total de ventas de los últimos 30 días(monto total y Q de ventas).
+        int totalVentas = datos.Sum(x => x.TotalLinea);
+        Console.WriteLine($"El total de ventas de los ultimos 30 días es: {totalVentas.ToString("N")}");
+
+        // Aqui con esta sencilla consulta seleccionamos toda la data de las siguientes 3 consultas, seleccionando
+        // el registro con el mayor monto total de ventas, una vez con este registro ya tenemos lo necesario para desplegar toda la informacion
+        var detalleDeVentasPorProductosYTotales = datos.OrderByDescending(x => x.IdVentaNavigation.Total).FirstOrDefault();
+
+        //El día y hora en que se realizó la venta con el monto más alto(y cuál es aquel monto).
+        Console.WriteLine($"La fecha y hora en que se realizo la venta mas grande fue " +
+        $"{detalleDeVentasPorProductosYTotales?.IdVentaNavigation.Fecha.ToString("yyyy-MM-dd HH:mm:ss")} y su monto es de {detalleDeVentasPorProductosYTotales?.IdVentaNavigation.Total.ToString("N")}");
+
+
+        //Indicar cuál es el producto con mayor monto total de ventas.
+        Console.WriteLine($"El producto con mayor monto total de ventas es: {detalleDeVentasPorProductosYTotales?.IdProductoNavigation.Nombre}");
+
+
+        //Indicar el local con mayor monto de ventas.
+        Console.WriteLine($"El local con mayor monto total de ventas es: {detalleDeVentasPorProductosYTotales?.IdVentaNavigation.IdLocalNavigation.Nombre}");
+
+        Console.ReadKey();
+
+
+    }
+}
